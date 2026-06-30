6c4af86bf53bcb469948388cb906f82b3cae0317
cherrykiwimango <allensaju7@gmail.com>
2026-06-30 19:34:40 +0530

    feat: create abuf struct which acts as a dynamic string to act as a dynamic buffer

--- kilo.c
+++ kilo.c
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <string.h>
 
 /*** defines ***/
 #define CTRL_KEY(k) ((k) & 0x1f)
@@ -90,6 +91,27 @@ int getWindowSize(int *rows, int *cols){
     *cols = ws.ws_col;
     return 0;
   }
+}
+
+/*** append buffer ***/
+struct abuf{
+  char *b;
+  int len;
+}
+
+#define ABUF_INIT {NULL, 0} 
+
+void abAppend(struct abuf *ab, char *s, int len){
+  char *new = realloc(ab->b, ab->len + len);
+  if(new == NULL) return;
+
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab){
+  free(ab->b);
 }
 
 /*** output ***/
