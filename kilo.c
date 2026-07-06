863dcdf35a86e839026e34ec2c63685150979632
cherrykiwimango <allensaju7@gmail.com>
2026-07-06 16:03:26 +0530

    feat: retain x position after snapping back to smaller lines

--- kilo.c
+++ kilo.c
@@ -38,7 +38,7 @@ typedef struct erow{
 } erow;
 
 struct editorConfig{
-  int cx, cy;
+  int cx, cy, verticalcx;
   int rowoff;
   int coloff;
   int screenrows;
@@ -297,6 +297,7 @@ void editorMoveCursor(int key){
     case ARROW_LEFT:
       if(E.cx != 0){
         E.cx--;
+        E.verticalcx = E.cx;
       }
       break;
     case ARROW_DOWN:
@@ -307,11 +308,13 @@ void editorMoveCursor(int key){
     case ARROW_RIGHT:
       if(row && E.cx < row->size){
         E.cx++;
+        E.verticalcx = E.cx;
       }
       break;
   } 
   row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
   int rowlen = row ? row->size : 0;
+  if(E.verticalcx != E.cx) E.cx = E.verticalcx;
   if(E.cx > rowlen) E.cx = rowlen;
 }
 
