4f0bc8ac5b5be15bc330df5229c432bd80a77971
cherrykiwimango <allensaju7@gmail.com>
2026-07-06 16:02:52 +0530

    feat: snap to end of line if the cursor extends beyond line size

--- kilo.c
+++ kilo.c
@@ -286,6 +286,8 @@ void editorRefreshScreen(){
 /*** input ***/
 
 void editorMoveCursor(int key){
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch(key){
     case ARROW_UP:
       if(E.cy != 0){
@@ -303,9 +305,14 @@ void editorMoveCursor(int key){
       }
       break;
     case ARROW_RIGHT:
-      E.cx++;
+      if(row && E.cx < row->size){
+        E.cx++;
+      }
       break;
   } 
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if(E.cx > rowlen) E.cx = rowlen;
 }
 
 void editorProcessKeyPress(){
