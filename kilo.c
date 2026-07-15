b8dee24196c3b4a6be6c4d0ded82269aedf057b8
cherrykiwimango <allensaju7@gmail.com>
2026-07-15 11:53:30 +0530

    feat: Add dirty flag to track whether file was modified or not

--- kilo.c
+++ kilo.c
@@ -52,6 +52,7 @@ struct editorConfig{
   int screenrows;
   int screencols;
   int numrows;
+  int dirty;
   erow *row;
   char *filename;
   char statusmsg[80];
@@ -222,6 +223,7 @@ void editorAppendRow(char *s, size_t len){
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c){
@@ -231,6 +233,8 @@ void editorRowInsertChar(erow *row, int at, int c){
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+
+  E.dirty++;
 }
 
 /*** editor operations ***/
@@ -283,6 +287,7 @@ void editorOpen(char *filename){
   }
   free(line);
   fclose(fp);
+  E.dirty = 0;
 }
 
 void editorSave(){
@@ -296,6 +301,7 @@ void editorSave(){
       if(write(fd, buf, len) == len){
         close(fd);
         free(buf);
+        E.dirty = 0;
         editorSetStatusMessage("%d bytes written to disk", len);
         return;
       }
@@ -386,7 +392,7 @@ void editorDrawRows(struct abuf *ab){
 void editorDrawStatusBar(struct abuf *ab){
   abAppend(ab, "\x1b[7m", 4);
   char status[80];
-  int len = snprintf(status, sizeof(status), " %.20s - %d lines", E.filename? E.filename : "[No Name]", E.numrows);
+  int len = snprintf(status, sizeof(status), " %.20s - %d lines %s", E.filename? E.filename : "[No Name]", E.numrows, E.dirty ? "- modified" : "");
   abAppend(ab, status, len);
   int endLen = snprintf(status, sizeof(status), "%d/%d ", E.cy+1, E.numrows);
   while (len < E.screencols - endLen) {
@@ -546,6 +552,7 @@ void initEditor(){
   E.rx = 0;
   E.rowoff= 0;
   E.numrows = 0;
+  E.dirty = 0;
   E.row = NULL;
   E.filename = NULL;
   E.statusmsg[0] = '\0';
